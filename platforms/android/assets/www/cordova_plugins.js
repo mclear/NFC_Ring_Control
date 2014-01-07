7199cfa6d254e022a14096dddf922ebfc5c05f96
John McLear <john@mclear.co.uk>
2014-01-07 21:00:46 +0000

    fix back button by usign latest cordova device plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,13 +6,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
         "id": "org.apache.cordova.dialogs.notification",
         "merges": [
@@ -32,15 +25,22 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "com.chariotsolutions.nfc.plugin": "0.4.7",
-    "org.apache.cordova.device": "0.2.6",
     "org.apache.cordova.dialogs": "0.2.5",
-    "org.apache.cordova.vibration": "0.3.6"
+    "org.apache.cordova.vibration": "0.3.6",
+    "org.apache.cordova.device": "0.2.7"
 }
 // BOTTOM OF METADATA
 });
--- platforms/wp8/.staging/www/cordova_plugins.js
+++ platforms/wp8/.staging/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
         "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
         "id": "org.apache.cordova.dialogs.notification",
         "merges": [
@@ -25,15 +18,22 @@ module.exports = [
         "file": "plugins/com.chariotsolutions.nfc.plugin/www/phonegap-nfc.js",
         "id": "com.chariotsolutions.nfc.plugin.NFC",
         "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.device": "0.2.6",
     "org.apache.cordova.dialogs": "0.2.5",
     "org.apache.cordova.vibration": "0.3.6",
-    "com.chariotsolutions.nfc.plugin": "0.4.7"
+    "com.chariotsolutions.nfc.plugin": "0.4.7",
+    "org.apache.cordova.device": "0.2.7"
 }
 // BOTTOM OF METADATA
 });
