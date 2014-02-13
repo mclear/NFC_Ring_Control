91d1b91377119075c699f131b455b7f65a477b36
John McLear <john@mclear.co.uk>
2014-02-13 03:21:52 +0000

    some more js, still need to include sharelocation code

--- www/js/nfcRing/heatmap.js
+++ www/js/nfcRing/heatmap.js
@@ -1,7 +1,65 @@
 nfcRing.heatmap = {
   coOrds: {}, // The Co-Ordinates we have stored for this phone
-  config: {}, // Our heatmap config
-  init: function(){}, // Initialize the heatmap
-  draw: function(){}, // Draw the data from our coOrds onto the heatmap
-  loadFromParse: function(){} // Load Heatmap data from parse
+
+  config: { // Our heatmap config
+    element: document.getElementById("heatMap"),
+    radius: 30,
+    opacity: 100
+  }, 
+  init: function(){
+    var heatmap = h337.create(config);
+    console.log("Writing data to heatmap", nfcRing.coOrdData);
+    heatmap.store.setDataSet(nfcRing.heatmap.coOrds);
+    console.log("Done writing data to the heatmap");
+  }, // Initialize the heatmap
+  draw: function(){ // Draw the data from our coOrds onto the heatmap
+
+  }, 
+
+  loadFromParse: function(){ // Getting data from Parse..
+    var TestObject = Parse.Object.extend("TestObject");
+    var testObject = new TestObject();
+    var query = new Parse.Query(TestObject);
+    try{
+      if(!device){return false;}
+    }catch(e){
+      return false;
+    }
+    query.equalTo("model", device.model);
+    console.log("Model", device.model);
+    try{
+      query.find({
+        success: function(results){
+          for (var i = 0; i < results.length; i++) { 
+            // TODO i18n this
+            $('#writeRing .inner > .actionName').html("<h2>Hold your NFC Ring to the back of your phone at the location indicated by the colored dots.</h2>");
+            var object = results[i];
+            var x = object.get('x');
+            var y = object.get('y');
+            // Turns it into a counted set of objects instead of single objects
+            coOrdinateCounter[x+":"+y] = coOrdinateCounter[x+":"+y] || 1;
+          }
+          if(results.length == 0){ // if there are no results
+            // TODO i18n this
+            $('#writeRing .inner > .actionName').html("<h2>It looks like our awesome community hasn't stored a location of the NFC Sweet Spot for your phone yet.</h2><p>Move the ring around the back of the phone until you recieve a confirmation.  This can take a little bit of time so be patient :)</p>");
+          }else{ // there are some heatmap results so let's draw em
+            console.log("Drawing heatmap");
+            nfcRing.coOrds = coOrdinateCounter;
+            // nfcRing.drawHeatMap();
+          }
+        },
+        failure: function(){
+          // fires on failure to connect
+          // TODO i18n this
+          $('#writeRing .inner > .actionName').html("We were unable to connect to our community sweet spot location servers, the sweet spot location will help you identify where to hold the ring on your phone.  You might need to turn on your Internet connection to access our servers, thanks");
+        }
+      });
+    }catch(e){
+      // Windows Phone wont do the Parse XHR request and fails on access is denied, this should be handled by the Parse API but it isn't so we handle it here
+      if(device.platform == "Win32NT"){
+        $('#writeRing .inner > .actionName').html("Windows Phone is unable to use our Community Sweet Spot location servers, we hope to get this fixed as soon as possible, sorry");
+        console.log("Unable to process Parse on WP");
+      }
+    }
+  }
 }
--- www/js/nfcRing/nfcEvent.js
+++ www/js/nfcRing/nfcEvent.js
@@ -28,8 +28,95 @@ nfcRing.nfcEvent = {
       console.log("NO NFC, SOMETHING IS WRONG HERE");
     }
   }, // create Event listeners
-  readOrWrite: function(nfcEvent){}, // Should we read or write to an NFC Event?
-  write: function(nfcEvent){}, // Write an NFC NDEf record
-  read: function(nfcEvent){}, // Read an NFC NDEF record
-  isValidURL: function(url){} // Is NFC NDEF Record a Valid URL
+  readOrWrite: function(nfcEvent){ // Should we read or write to an NFC Event?
+    $('#message').hide(); // hide help message
+    if (nfcRing.toWrite) {
+      console.log("Doing write event", nfcEvent);
+      nfcRing.write(nfcEvent);
+      $('#writeRing').show();
+    } else {
+      nfcRing.read(nfcEvent);
+    }
+  },
+  write: function(nfcEvent){ // Write an NFC NDEf record
+    // If the string is a valid URL
+    var isURL = nfcRing.validURL(nfcRing.toWrite);
+
+    if (isURL) {
+      console.log("URL Record");
+      var ndefRecord = ndef.uriRecord(nfcRing.toWrite); // Creates a URI record
+    } else {
+      console.log("Text record");
+      // The string must be a text record as that's the only other type we support
+      var ndefRecord = ndef.textRecord(nfcRing.toWrite); // Creates a Text record
+    }
+
+    nfc.write([ndefRecord], function () {
+      navigator.notification.vibrate(100);
+      console.log("Written", ndefRecord);
+      var dontAskSweetSpotAgain = localStorage.getItem("dontAskSweetSpotAgain");
+      console.log("dontAskSweetSpotAgain", dontAskSweetSpotAgain);
+  
+      if (dontAskSweetSpotAgain === "true") { // we should ask for the sweet spot
+        alert("Your ring is now ready", false, "Woohoo!");
+      } else {
+        var shareLocation = confirm("Your ring is ready.  Would you like to be awesome and help others by sharing the sweet spot location for this phone model? ", false, "Done, Thank you!");
+      }
+
+      console.log("Share location response", shareLocation);
+      if (shareLocation) {
+        console.log("Set localstorage item dont ask sweet spot again");
+        localStorage.setItem("dontAskSweetSpotAgain", true);
+        console.log("nfcEvent", nfcEvent)
+        if (!nfcEvent) nfcEvent = {}; // Hack as WP8 doesn't pass event always
+        if (!nfcEvent.tag) nfcEvent.tag = {}; // Continued Hack
+        if (nfcEvent.tag.id) {
+          var idStr = nfcEvent.tag.id.join(",");
+        } else {
+          var idStr = "false";
+        }
+        window.location = "shareLocation.html#?guid=" + idStr;
+      } else {
+        localStorage.setItem("dontAskSweetSpotAgain", true);
+      }
+
+      // This may seem crazy but WP needs to continue to call this but we should wait two seconds
+      if (device.platform === "Win32NT") {
+        setTimeout(function () {
+          nfcRing.write(false)
+        }, 2000);
+      }
+
+    }, function (reason) {
+      console.log("Inlay write failed");
+      // This may seem crazy but WP needs to continue to call this..
+      if (device.platform === "Win32NT") {
+        nfcRing.write(false)
+      }
+    });
+  }, 
+  read: function(nfcEvent){ // Read an NFC NDEF record
+    console.log("Reading", nfcEvent);
+    var ring = nfcEvent.tag;
+    if(ring.ndefMessage){
+      ringData = nfc.bytesToString(ring.ndefMessage[0].payload); // TODO make this less fragile 
+      console.log(ringData, false, "Ring contents:");
+      alert(ringData, false, "Ring contents:");
+    }else{
+      alert("No NDEF data found", false, "Unable to read");
+    }
+  },
+  isValidURL: function(url){ // Is NFC NDEF Record a Valid URL
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    if (!pattern.test(url)) {
+      return false;
+    } else {
+      return true;
+    }    
+  }
 }
