e18d59ffd7c700d610793dc3e2c63f81bf8d5575
John McLear <john@mclear.co.uk>
2014-06-17 14:32:55 +0100

    working read

--- www/js/nfcRing/heatmap.js
+++ www/js/nfcRing/heatmap.js
@@ -6,7 +6,7 @@ nfcRing.heatmap = {
 
   init: function(){
 
-    nfcRing.heatmap.loadFromParse(function(){
+    nfcRing.heatmap.loadFromRemote(function(){
 
       if($('#heatMap canvas').length >= 1){
         console.log("Heatmap already drawn");
@@ -51,53 +51,40 @@ nfcRing.heatmap = {
 
   }, 
 
-  loadFromParse: function(callback){ // Getting data from Parse..
-    parseInitSS();
-    var coOrdinateCounter = {};
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    var query = new Parse.Query(TestObject);
+  loadFromRemote: function(callback){ // Getting data from Parse..
+
     try{
       if(!device){return false;}
     }catch(e){
       return false;
     }
-    query.equalTo("model", device.model);
-    console.log("Model", device.model);
-    try{
-      query.find({
-        success: function(results){
-          for (var i = 0; i < results.length; i++) { 
-            var object = results[i];
-            var x = object.get('x');
-            var y = object.get('y');
-            // Turns it into a counted set of objects instead of single objects
-            coOrdinateCounter[x+":"+y] = coOrdinateCounter[x+":"+y] || 1;
-            console.log("Got results from Parse", coOrdinateCounter);
-          }
-          if(results.length == 0){ // if there are no results
-            console.log("no results from parse");
-          }else{ // there are some heatmap results so let's draw em
-            console.log("0. Drawing heatmap");
-            if($('#heatMap canvas').length < 1){
-              nfcRing.heatmap.coOrds = coOrdinateCounter;
-            }
-            // nfcRing.drawHeatMap();
-            callback();
-          }
-        },
-        failure: function(){
-          // fires on failure to connect
-          $('#writeRingTitle').html(html10n.get('sweetSpot.unableToConnect'));
-        }
-      });
-    }catch(e){
-      // Windows Phone wont do the Parse XHR request and fails on access is denied, this should be handled by the Parse API but it isn't so we handle it here
-      if(device.platform === "Win32NT"){
-        $('#writeRingTitle').html("Windows Phone is unable to use our Community Sweet Spot location servers, we hope to get this fixed as soon as possible, sorry");
-        console.log("Unable to process Parse on WP");
+
+    var coOrdinateCounter = {};
+    var model = device.model;
+
+    $.getJSON("http://sweetspot.nfcring.com/api/v1/sweetspot?model="+model).done(function(results){
+      results = JSON.parse(results);
+      for (var i = 0; i < results.length; i++) { 
+        var object = results[i];
+        var x = object.x;
+        var y = object.y;
+        // Turns it into a counted set of objects instead of single objects
+        coOrdinateCounter[x+":"+y] = coOrdinateCounter[x+":"+y] || 1;
       }
-    }
+      console.log("Got results from Parse", coOrdinateCounter);
+
+      if(results.length == 0){ // if there are no results
+        console.log("no results from parse");
+      }else{ // there are some heatmap results so let's draw em
+        console.log("0. Drawing heatmap");
+        if($('#heatMap canvas').length < 1){
+          nfcRing.heatmap.coOrds = coOrdinateCounter;
+        }
+        // nfcRing.drawHeatMap();
+        callback();
+      }
+
+    });
   },
 
   sendToParse: function(x,y,phoneModel){
