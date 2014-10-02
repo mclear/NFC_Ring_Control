a6dca88ee1f07c353396c37d17f55dcd2633e557
John McLear <john@mclear.co.uk>
2014-10-02 16:21:58 +0100

    bump webintent plugin and fix issue w/ wrong namespace being used

--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,52 @@ var app = {
   },
   bind: function () {
     document.addEventListener('deviceready', this.deviceready, false);
+    document.addEventListener('resume', this.deviceready, false);
   },
+  resume: function () {
+    // BELOW NEEDS A REFACTOR
+    if(typeof cordova !== 'undefined'){
+      console.log("Checking for intent");
+      webintent.hasExtra(webintent.EXTRA_TEXT,
+        function(hasExtra) {
+          if(hasExtra){
+            console.log("Intent passed, handling that way");
+            webintent.getExtra(webintent.EXTRA_TEXT, function(value) {
+              if(!value) value = "http://nfcring.com/getStarted";
+              console.log("Intent value is ", value);
+              nfcRing.userValues.activity = "write";
+              nfcRing.userValues.intentSet = true;
+              nfcRing.userValues.toWrite = value;
+              setTimeout(function(){
+                nfcRing.heatmap.init();
+                nfcRing.ui.displayPage("writeRing");
+                nfcRing.ui.prepareWritePage("write");
+              }, 500);
+            }, function(){
+              console.log("ERROR XVMA123");
+            });
+          }
+        }, function() {
+          console.log("ERROR XVMA172");
+        }
+      );
+    }
+
+    webintent.onNewIntent(function(intent, test) {
+
+      console.log("new intent event detected", intent, test);
+
+      webintent.hasExtra(webintent.EXTRA_TEXT,
+        function(hasExtra) {
+          if(hasExtra){
+            console.log("Intent passed, handling that way");
+          }
+      });
+
+    });
+
+  },
+  
   deviceready: function () {
 
     // See http://docs.phonegap.com/en/edge/cordova_notification_notification.md.html#Notification
@@ -28,11 +73,11 @@ var app = {
     // BELOW NEEDS A REFACTOR
     if(typeof cordova !== 'undefined'){
       console.log("Checking for intent");
-      CDV.WEBINTENT.hasExtra(CDV.WEBINTENT.EXTRA_TEXT,
+      webintent.hasExtra(webintent.EXTRA_TEXT,
         function(hasExtra) {
           if(hasExtra){
             console.log("Intent passed, handling that way");
-            CDV.WEBINTENT.getExtra(CDV.WEBINTENT.EXTRA_TEXT, function(value) {
+            webintent.getExtra(webintent.EXTRA_TEXT, function(value) {
               if(!value) value = "http://nfcring.com/getStarted";
               console.log("Intent value is ", value);
               nfcRing.userValues.activity = "write";
@@ -53,11 +98,11 @@ var app = {
       );
     }
 
-    CDV.WEBINTENT.onNewIntent(function(intent, test) {
+    webintent.onNewIntent(function(intent, test) {
 
       console.log("new intent event detected", intent, test);
 
-      CDV.WEBINTENT.hasExtra(CDV.WEBINTENT.EXTRA_TEXT,
+      webintent.hasExtra(webintent.EXTRA_TEXT,
         function(hasExtra) {
           if(hasExtra){
             console.log("Intent passed, handling that way");
