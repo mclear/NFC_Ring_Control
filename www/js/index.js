de15c87dcf2e3492e29f05015778639b60971768
John McLear <john@mclear.co.uk>
2014-10-02 17:13:32 +0100

    refactor

--- www/js/index.js
+++ www/js/index.js
@@ -6,9 +6,7 @@ var app = {
     document.addEventListener('deviceready', this.deviceready, false);
     document.addEventListener('resume', this.resume, false);
   },
-  resume: function () {
-    // BELOW NEEDS A REFACTOR
-    console.log("RESUMING!");
+  webintentListener: function () {
     if(typeof cordova !== 'undefined'){
       console.log("Checking for intent", webintent);
       webintent.hasExtra(webintent.EXTRA_TEXT,
@@ -38,20 +36,21 @@ var app = {
 
     webintent.onNewIntent(function(intent, test) {
       console.log("new intent event detected", intent, test);
-      webintent.hasExtra(webintent.EXTRA_TEXT,
-        function(hasExtra) {
-          if(hasExtra){
-            console.log("Intent passed, handling that way");
-          }
+      webintent.hasExtra(webintent.EXTRA_TEXT, function(hasExtra) {
+        if(hasExtra){
+          console.log("Intent passed, handling that way");
+        }
       });
-
     });
 
     //handle app invoke via activity
     window.plugins.webintent.getUri(function(invokeUrl) {
       console.log("[INTENT] handleAndroidOpen: " +  invokeUrl);
     });
-
+  },
+  resume: function () {
+    console.log("Resuming");
+    app.webintentListener();
   },
 
   deviceready: function () {
@@ -74,51 +73,8 @@ var app = {
 
     FastClick.attach(document.body);
 
-    // BELOW NEEDS A REFACTOR
-    if(typeof cordova !== 'undefined'){
-      console.log("Checking for intent", webintent);
-      webintent.hasExtra(webintent.EXTRA_TEXT,
-        function(hasExtra) {
-          if(hasExtra){
-            console.log("Intent passed, handling that way");
-            webintent.getExtra(webintent.EXTRA_TEXT, function(value) {
-              if(!value) value = "http://nfcring.com/getStarted";
-              console.log("Intent value is ", value);
-              nfcRing.userValues.activity = "write";
-              nfcRing.userValues.intentSet = true;
-              nfcRing.userValues.toWrite = value;
-              setTimeout(function(){
-                nfcRing.heatmap.init();
-                nfcRing.ui.displayPage("writeRing");
-                nfcRing.ui.prepareWritePage("write");
-              }, 500);
-            }, function(){
-              console.log("ERROR XVMA123");
-            });
-          }
-        }, function() {
-          console.log("ERROR XVMA172");
-        }
-      );
-    }
-
-    webintent.onNewIntent(function(intent, test) {
-
-      console.log("new intent event detected", intent, test);
-
-      webintent.hasExtra(webintent.EXTRA_TEXT,
-        function(hasExtra) {
-          if(hasExtra){
-            console.log("Intent passed, handling that way");
-          }
-      });
-
-    });
-
-    //handle app invoke via activity
-    window.plugins.webintent.getUri(function(invokeUrl) {
-      console.log("[INTENT] handleAndroidOpen: " +  invokeUrl);
-    });
+    // Check to see if intent is available
+    app.webintentListener();
 
   }
 };
