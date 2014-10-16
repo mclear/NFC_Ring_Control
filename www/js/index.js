41d6c8d5b6c7b951238897c521bd97f261b26530
John McLear <john@mclear.co.uk>
2014-10-16 16:00:28 +0100

    bumpage

--- www/js/index.js
+++ www/js/index.js
@@ -4,35 +4,16 @@ var app = {
   },
   bind: function () {
     document.addEventListener('deviceready', this.deviceready, false);
+    document.addEventListener('resume', this.resume, false);
   },
-  deviceready: function () {
-
-    // See http://docs.phonegap.com/en/edge/cordova_notification_notification.md.html#Notification
-    alert = navigator.notification.alert;
-    prompt = navigator.notification.prompt;
-    confirm = navigator.notification.confirm;
-
-    // Begin listening for NFC Tags
-    nfcRing.nfcEvent.init();
-
-    if (device.platform == "Win32NT") {
-      $('#read').hide();
-      $('.win32').show(); // Note to Designer, by default this needs to be hidden
-    }
-
-    // Handle back events
-    document.addEventListener("backbutton", nfcRing.ui.handleBack, false);
-
-    FastClick.attach(document.body);
-
-    // BELOW NEEDS A REFACTOR
+  webintentListener: function () {
     if(typeof cordova !== 'undefined'){
-      console.log("Checking for intent");
-      CDV.WEBINTENT.hasExtra(CDV.WEBINTENT.EXTRA_TEXT,
+      console.log("Checking for intent", webintent);
+      webintent.hasExtra(webintent.EXTRA_TEXT,
         function(hasExtra) {
           if(hasExtra){
             console.log("Intent passed, handling that way");
-            CDV.WEBINTENT.getExtra(CDV.WEBINTENT.EXTRA_TEXT, function(value) {
+            webintent.getExtra(webintent.EXTRA_TEXT, function(value) {
               if(!value) value = "http://nfcring.com/getStarted";
               console.log("Intent value is ", value);
               nfcRing.userValues.activity = "write";
@@ -53,17 +34,47 @@ var app = {
       );
     }
 
-    CDV.WEBINTENT.onNewIntent(function(intent, test) {
-
+    webintent.onNewIntent(function(intent, test) {
       console.log("new intent event detected", intent, test);
-
-      CDV.WEBINTENT.hasExtra(CDV.WEBINTENT.EXTRA_TEXT,
-        function(hasExtra) {
-          if(hasExtra){
-            console.log("Intent passed, handling that way");
-          }
+      webintent.hasExtra(webintent.EXTRA_TEXT, function(hasExtra) {
+        if(hasExtra){
+          console.log("Intent passed, handling that way");
+        }
       });
-
     });
+
+    //handle app invoke via activity
+    window.plugins.webintent.getUri(function(invokeUrl) {
+      console.log("[INTENT] handleAndroidOpen: " +  invokeUrl);
+    });
+  },
+  resume: function () {
+    console.log("Resuming");
+    app.webintentListener();
+  },
+
+  deviceready: function () {
+
+    // See http://docs.phonegap.com/en/edge/cordova_notification_notification.md.html#Notification
+    alert = navigator.notification.alert;
+    prompt = navigator.notification.prompt;
+    confirm = navigator.notification.confirm;
+
+    // Begin listening for NFC Tags
+    nfcRing.nfcEvent.init();
+
+    if (device.platform == "Win32NT") {
+      $('#read').hide();
+      $('.win32').show(); // Note to Designer, by default this needs to be hidden
+    }
+
+    // Handle back events
+    document.addEventListener("backbutton", nfcRing.ui.handleBack, false);
+
+    FastClick.attach(document.body);
+
+    // Check to see if intent is available
+    app.webintentListener();
+
   }
 };
