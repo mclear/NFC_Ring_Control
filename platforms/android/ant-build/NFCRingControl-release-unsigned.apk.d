0f233028a023d0179ccbc8098b91ba90a565c6f5
John McLear <john@mclear.co.uk>
2014-12-17 22:30:54 +0000

    apks

--- platforms/android/ant-build/NFCRingControl-release-unsigned.apk.d
+++ platforms/android/ant-build/NFCRingControl-release-unsigned.apk.d
@@ -1,4 +1,4 @@
-/var/www/NFC_Ring_Control/platforms/android/ant-build/NFCRingControl-release-unsigned.apk : \
-/var/www/NFC_Ring_Control/platforms/android/ant-build/NFCRingControl.ap_ \
-/var/www/NFC_Ring_Control/platforms/android/ant-build/classes.dex \
-/var/www/NFC_Ring_Control/platforms/android/CordovaLib/ant-build/classes.jar \
+/var/www/html/NFC_Ring_Control/platforms/android/ant-build/NFCRingControl-release-unsigned.apk : \
+/var/www/html/NFC_Ring_Control/platforms/android/ant-build/NFCRingControl.ap_ \
+/var/www/html/NFC_Ring_Control/platforms/android/ant-build/classes.dex \
+/var/www/html/NFC_Ring_Control/platforms/android/CordovaLib/ant-build/classes.jar \
