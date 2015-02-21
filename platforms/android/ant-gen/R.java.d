49b4db584bcc04578438ad2221cf21a10be81591
John McLear <john@mclear.co.uk>
2015-02-21 19:53:02 +0000

    final commits make sure everything is tidy

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,11 +6,9 @@
 /home/jose/NFC_Ring_Control/platforms/android/res/drawable-hdpi/icon.png \
 /home/jose/NFC_Ring_Control/platforms/android/res/values/strings.xml \
 /home/jose/NFC_Ring_Control/platforms/android/res/xml/config.xml \
-/home/jose/NFC_Ring_Control/platforms/android/ant-build/res/drawable-xxhdpi/icon.png \
 /home/jose/NFC_Ring_Control/platforms/android/ant-build/res/drawable-mdpi/icon.png \
 /home/jose/NFC_Ring_Control/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /home/jose/NFC_Ring_Control/platforms/android/ant-build/res/drawable/icon.png \
 /home/jose/NFC_Ring_Control/platforms/android/ant-build/res/drawable-ldpi/icon.png \
 /home/jose/NFC_Ring_Control/platforms/android/ant-build/res/drawable-hdpi/icon.png \
-/home/jose/NFC_Ring_Control/platforms/android/ant-build/res/drawable-xxxhdpi/icon.png \
 /home/jose/NFC_Ring_Control/platforms/android/ant-build/AndroidManifest.xml \
